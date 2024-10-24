c04ff98b9700d1b418a1b25e96c004269c801a63
aaronalby <ssnaaron10@gmail.com>
2024-10-24 11:36:21 +0530

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -1,78 +1,60 @@
 import java.util.Scanner;
 
-public class QuickSort
+public class QuickSort {
 
-static int Partition(int A[],int lb,int ub) (
+    static int partition(int A[], int lb, int ub) {
+        int pivot = A[ub];
+        int i = lb - 1;
 
-int pivot=A[ub];
+        for (int j = lb; j < ub; j++) {
+            if (A[j] <= pivot) {
+                i++;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
 
-int i = 1b - 1
+        int temp = A[i + 1];
+        A[i + 1] = A[ub];
+        A[ub] = temp;
 
-for(int j=lb;j<ub;j++) {
+        return (i + 1);
+    }
 
-if(A[j]<=pivot){
+    static void quicksort(int A[], int lb, int ub) {
+        if (lb < ub) {
+            int loc = partition(A, lb, ub);
+            quicksort(A, lb, loc - 1);
+            quicksort(A, loc + 1, ub);
+        }
+    }
 
-int temp=A[i];
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-A[i]=A[j]:
+        System.out.print("Enter the number of elements: ");
+        int len = s.nextInt();
+        int num[] = new int[len];
 
-A[j] = 1 temp;
+        System.out.println("Length of the array is: " + len);
 
-int temp = A[i + 1]
+        for (int i = 0; i < len; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            num[i] = s.nextInt();
+        }
 
-A[i + 1] = A[ub]
+        System.out.println("Unsorted Array: ");
+        for (int i = 0; i < len; i++) {
+            System.out.print(num[i] + " ");
+        }
+        System.out.println("\n");
 
-A[ub] = temp;
+        quicksort(num, 0, len - 1);
 
-return (i + 1)
-
-static void quicksort(int A[],int lb,int ub) {
-
-if(lb<ub) {
-
-int loc-Partition(A,lb,ub);
-
-quicksort(A, lb,loc-1);
-
-quicksort(A,loc+1,ub);
-
-}
-
-public static void main(String[] args) {
-
-int lb,ub;
-
-Scanner s=new Scanner(System.in);
-
-System.out.print("Enter the number of elements: "); int len-s.nextInt();
-
-int num[]=new int[len];
-
-System.out.println("Length of the array is: "+len);
-
-for(int i = 0 i<len;i++) {
-
-System.out.println("Enter the elements of the array: ");
-
-num[i]=s.nextInt();
-
-}
-System.out.println("Unsorted Array: ");
-
-for(int i=0;i<len;i++) {
-
-System.out.print(num[i]+" ");
-
-}
-
-System.out.println("\n");
-
-quicksort(num,0,len-1);
-
-System.out.println("Sorted Array: ");
-
-for(int i=0;i<len;i++) {
-
-System.out.print(num[i]+" ");
-
+        System.out.println("Sorted Array: ");
+        for (int i = 0; i < len; i++) {
+            System.out.print(num[i] + " ");
+        }
+    }
 }
